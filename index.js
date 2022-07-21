1e1826cbf101342cdde9dad85d8232c615a8b85f
kmrussel <Katiemrussell28@gmail.com>
2022-07-21 02:29:25 -0700

    added languages.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const langModule = require('./languages.js')
 const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
@@ -6,21 +7,31 @@ const express = require('express');
 const PORT = 8000
 const app = express()
 
-const url = 'https://en.wikipedia.org/wiki/' +'French_language'
+let language = langModule.randomLanguage();
+const url = 'https://en.wikipedia.org/wiki/' + language
 
-// generates a random number correlating to p attribute # in overview (between 3-5)
-const randNum = Math.floor(Math.random() * (5 - 3 + 1) + 3)
-
-// scrapes random paragraph from wikipedia article  
-const getRandomPara = async (url, randNum) => {
+// scrapes overview from wikipedia article 
+const getOverview = async (url) => {
     try{
         const res = await axios.get(url)
         const html =  await res.data
         const $ = cheerio.load(html)
-        const $para = $(`.mw-parser-output > p:nth-of-type(${randNum})`);
+       
+        // gets first instance of p 
+        let i = 1 
+        while ($(`.mw-parser-output > p:nth-of-type(${i})`).text().trim() === "") {
+            i++ 
+        }
+        
+        // scrapes until no longer p 
+        let $para =  $(`.mw-parser-output > p:nth-of-type(${i})`).prev()
+        $para = $para.nextUntil(":not(p)")
+      
+        // removes references and formats into text 
         $para.find('sup').remove()
         const randPara = $para.text().trim()
-        return randPara
+        return(randPara)
+    
     } catch (error) {
         console.log(error)
     }
@@ -28,17 +39,27 @@ const getRandomPara = async (url, randNum) => {
 }
 
 // https://stackoverflow.com/questions/18914629/split-string-into-sentences-in-javascript
+// credit!
+
     
+const randSentence = getOverview(url).then((overview) => {
+    // separates sentences in overview and inserts into an array
+    const sentences = overview.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|");
     
-const randSentence = getRandomPara(url, randNum).then((paragraph) => {
-    const sentences = paragraph.replace(/([.?!])\x20{1,2}(?=[A-Z\d])/g, "$1|").split("|");
+    // picks a random sentences from the array
     const len = sentences.length;
     const randNum = Math.floor(Math.random() * len);
     return sentences[randNum]
+}).catch((error) => {
+    console.log(error)
+    return ('Please try again')
 });
 
-// this returns a pending promise fix ;-; 
 
-randSentence.then((result) => {console.log(result)} )
+randSentence.then((result) => {
+    language = language.replace(/_/g, ' ')
+    console.log(language + ': ' + result)
+    // send as an object { language: result }
+});
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
--- languages.js
+++ languages.js
@@ -1,4 +1,5 @@
 
+// The following languages are excluded because they have no page or nothing is on their page: chichewa, hmong, shona, sundanese
 
 const languages = ['Afrikaans', 'Albanian_language', 'Amharic', 'Arabic',
 'Armenia', 'Azerbaijani_language', 'Basque_language', 'Belarusian_language', 'Bengali_language',
@@ -6,6 +7,24 @@ const languages = ['Afrikaans', 'Albanian_language', 'Amharic', 'Arabic',
  'Corsican_language', 'Croatian_language', 'Czech_language', 'Danish_language', 'Dutch_language',
 'English_language', 'Esperanto', 'Estonian_language', 'Filipino_language', 'Finnish_language',
 'French_language', 'Galician_language', 'Georgian_language', 'German_language',
- 'Greek_language', '']
+ 'Greek_language', 'Gujarati_language', 'Haitian_Creole', 'North_Frisian_language',
+'Hausa_language', 'Hawaiian_language', 'Hebrew_language', 'Hindi', 'Hungarian_language'
+,'Icelandic_language', 'Igbo_language', 'Indonesian_language', 'Irish_language', 'Italian_language',
+'Japanese_language', 'Javanese_language', 'Kannada', 'Kazakh_language', 'Khmer_language',
+'Korean_language', 'Kurdish_language', 'Kyrgyz_language', 'Lao_language', 'Latvian_language',
+'Lithuanian_language', 'Luxembourgish_language', 'Macedonian_language', 'Malagasy_language',
+'Malay_language', 'Malayalam', 'Maltese_language', 'Maori_language', 'Marathi_language', 'Mongolian_language',
+'Burmese_language', 'Nepali_language', 'Norwegian_language', 'Odia_language', 'Pashto_language',
+'Persian_language', 'Polish_language', 'Portuguese_language', 'Punjabi_language', 'Romanian_language',
+'Russian_language', 'Samoan_language', 'Scottish_Gaelic_language', 'Serbian_language', 'Southern_Sotho',
+'Sindhi_language', 'Sinhala_language', 'Slovak_language', 'Slovene_language', 'Somali_language',
+'Spanish_language', 'Swahili_language', 'Swedish_language', 'Tajik_language','Tamil_language',
+'Telugu', 'Thai_language', 'Turkish_language', 'Ukrainian_language', 'Urdu', 'Uyghur_language', 'Uzbek_language',
+'Vietnamese_language', 'Welsh_language', 'Xhosa_language', 'Yoruba_language', 'Zulu_language']
 
-//chichewa, frisian, gujarati
+module.exports = {randomLanguage: function() {
+        const len = languages.length;
+        const randLang = Math.floor(Math.random() * len);
+        return languages[randLang]
+    }
+}
